5f72b87b5369afe398008d7fe66ec4dc3085cfaf
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-01-08 20:48:46 -0400

    (update) Atualização na função MacAddressChanger.

--- src/functions.sh
+++ src/functions.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-pasta="$HOME/Downloads-ScriptLinuxUtils"
+# pasta="$HOME/Downloads-ScriptLinuxUtils"
 
 function proxyfirefox(){
-    proxychain firefox
+    proxychains firefox
 }
 
 function MacAddressChanger(){
     show=$(ip link show eth0 | grep ether | awk '{print $2}')
     echo "Mudando MAC Address..."
-    echo "/// Seu MAC Address atual: $show///"
-    echo .
-    echo .
+    echo "/\ Seu MAC Address atual: $show"
     sudo ip link set eth0 down
     sudo macchanger -A
     sudo ip link set eth0 up
-    echo .
-    echo .
-    echo "/// Seu novo MAC Address: $(ip link show eth0 | grep ether | awk '{print $2}') ///"
+    echo "/\ Seu novo MAC Address: $(ip link show eth0 | grep ether | awk '{print $2}') ///"
 }
