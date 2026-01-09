d56cd5b38d4740582db50b6ba555dce5c9a7507d
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-01-08 20:50:26 -0400

    (update) Remoção da função de configuração do proxychain4, utilizando configuração direta de script sem recursão; Instalação e ativação do Atuin;

--- src/KaliLinuxConfig.sh
+++ src/KaliLinuxConfig.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-# Funcao de configuracao do proxychains para o funcionamento do Tor Browser
-# Evita que o seu IP real seja vazado e caia na lista negra de IPs
-# CONFIGURACAO PADRAO, MAS NAO RECOMENDADA, EM CASO DE USO DE VPN
-function proxyconfig(){
-    {
-        echo "dynamic_chain"
-        echo "proxy_dns"
-        echo "remote_dns_subnet 224"
-        echo "tcp_read_time_out 15000"
-        echo "tcp_connect_time_out 8000"
-        echo "[ProxyList]"
-        echo "socks4 127.0.0.1:9050"
-        echo "socks5 127.0.0.1:9050"
-
-    } > /etc/proxychains4.conf
-}
+packages=( tor cargo htop python3 pip macchanger plocate fastfetch fzf )
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt-get full-upgrade -y
 
-sudo apt-get autoclean
+sudo apt-get autoclean -y
 
-packages=( curl tor wget htop python3 pip macchanger )
+rm /etc/proxychains4.conf
+touch /etc/proxychains4.conf
+
+# Funcao de configuracao do proxychains para o funcionamento do Tor Browser
+# Evita que o seu IP real seja vazado e caia na lista negra de IPs
+# CONFIGURACAO PADRAO, MAS NAO RECOMENDADA, EM CASO DE USO DE VPN
+{
+    echo "dynamic_chain"
+    echo "proxy_dns"
+    echo "remote_dns_subnet 224"
+    echo "tcp_read_time_out 15000"
+    echo "tcp_connect_time_out 8000"
+    echo "[ProxyList]"
+    echo "socks4 127.0.0.1:9050"
+    echo "socks5 127.0.0.1:9050"
+
+} > /etc/proxychains4.conf
+
 sudo apt install -y "${packages[@]}"
 
 # Configuracao do Tor
 echo 'GRUB_DISABLE_RECOVERY="true"' >> /etc/default/grub
-proxyconfig
 
+# Habilitando o tor para inicio automatico com o sistema
 sudo service tor start
 sudo systemctl enable tor
+
+# Instalando e habilitando o Atuin
+cargo install atuin
+echo 'eval "$(atuin init bash)"' >> ~/.bashrc
